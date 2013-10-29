dfeaf0de470ccdcb9ee9a55368ef5c59957cf512
Astrolitterbox <opit@hardcore.lt>
2013-10-29 12:42:53 +0100

    data

--- make_data_table.py
+++ make_data_table.py
@@ -8,7 +8,7 @@ califa_id = data[:, 0]
 #lum_err = 10*np.abs(data[:, 3]) + 2
 #vel_err = 100*np.abs(np.log10(np.abs(data[:, 4]))) + 2
 
-log_vel = np.log10(np.abs(data[:, 1]))
+log_vel = np.log10(0.4*np.abs(data[:, 1]))
 
 lum_err = np.abs(data[:, 3]) 
 vel_err = np.abs(np.log10(np.abs(data[:, 4]))) 
@@ -17,7 +17,7 @@ lum = data[:, 2]
 out = np.zeros((data.shape[0], 7))
 out[:, 0] = califa_id
 out[:, 1] = log_vel
-out[:, 2] = np.abs(lum)
+out[:, 2] = lum
 out[:, 3] = lum_err
 out[:, 4] = vel_err
 
